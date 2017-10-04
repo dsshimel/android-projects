4731fdba21d724c21e5e76e1033b8cecf5809cf6
dsshimel <dsshimel@gmail.com>
2017-10-03 22:26:51 -0400

    first working version

--- CoffeeCounter/app/src/main/java/com/davidshimel/coffeecounter/Counter.java
+++ CoffeeCounter/app/src/main/java/com/davidshimel/coffeecounter/Counter.java
@@ -1,6 +1,7 @@
 package com.davidshimel.coffeecounter;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +10,21 @@ public class Counter {
 
     private List<DateTime> coffees = new ArrayList<>();
 
-    public DateTime count() {
+    DateTime drinkCoffee() {
         DateTime coffeeInstant = DateTime.now();
         coffees.add(coffeeInstant);
         return coffeeInstant;
+    }
+
+    int getNumCoffees() {
+        return coffees.size();
+    }
+
+    List<String> getCoffeeTimeStringArray() {
+        ArrayList<String> coffeeTimes = new ArrayList<>();
+        for (DateTime coffeeTime : coffees) {
+            coffeeTimes.add(coffeeTime.toString(DateTimeFormat.shortTime()));
+        }
+        return coffeeTimes;
     }
 }
--- CoffeeCounter/app/src/main/java/com/davidshimel/coffeecounter/MainActivity.java
+++ CoffeeCounter/app/src/main/java/com/davidshimel/coffeecounter/MainActivity.java
@@ -4,14 +4,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 public class MainActivity extends AppCompatActivity {
     private final Counter coffeeCounter;
+    private ArrayAdapter<String> coffeeArrayAdapter;
 
     public MainActivity() {
         coffeeCounter = new Counter();
@@ -22,16 +27,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         JodaTimeAndroid.init(this);
         setContentView(R.layout.activity_main);
-
         Button drinkButton = (Button) findViewById(R.id.drink_button);
         drinkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DateTime coffeeInstant = coffeeCounter.count();
-                Snackbar.make(findViewById(R.id.main_view),
-                        "You drank a coffee at " + coffeeInstant.toString(),
-                        Snackbar.LENGTH_SHORT).show();
+                DateTime coffeeInstant = coffeeCounter.drinkCoffee();
+                String coffeeInstantString = coffeeInstant.toLocalTime().toString(DateTimeFormat.shortTime());
+                updateStatsText();
+                updateCoffeeList(coffeeInstantString);
             }
         });
+
+        coffeeArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, coffeeCounter.getCoffeeTimeStringArray());
+        ((ListView) findViewById(R.id.drink_list)).setAdapter(coffeeArrayAdapter);
+    }
+
+    private void updateStatsText() {
+        int numCoffees = coffeeCounter.getNumCoffees();
+        ((TextView) findViewById(R.id.stats)).setText(
+                getResources().getQuantityString(R.plurals.coffee_count, numCoffees, numCoffees));
+    }
+
+    private void updateCoffeeList(String coffeeInstantString) {
+        coffeeArrayAdapter.add(coffeeInstantString);
     }
 }
